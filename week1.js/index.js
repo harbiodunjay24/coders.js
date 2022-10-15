1abb57882f4227e2e9ee40dc4cc8b965c59becca
harbiodunjay24 <harbiodunjay24@gmail.com>
2022-10-15 02:06:30 -0700

    first js

---
+++ week1.js/index.js
@@ -0,0 +1,70 @@
+// 1.Write a single line comment which says, comments can make code readable
+// comments can make code readable
+
+// 2.Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// 4.Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*comments can make code readable, 
+easy to reuse and informative*/
+
+// 5.Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let name="abiodun"; 
+
+let firstname
+console.log(firstname);
+
+let city=null
+console.log(city);
+
+let ismarried=true
+console.log(ismarried);
+
+// 6.Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof "ade");
+
+console.log(typeof FirstName);
+
+console.log(typeof null)
+
+console.log(typeof false) 
+
+console.log(typeof 6) 
+
+// Declare four variables without assigning values
+
+let age =null
+
+const address=null
+
+let housenum=null
+
+let biodun=null
+
+
+// Declare four variables with assigned values
+
+let ages ="6"
+const mname = "ayo"
+let sex = "male"
+let statuss ="working"
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let Age ="6"
+const First_Name = "ayo"
+const Last_Name = "abbey"
+let country = "nigeria"
+let marital_status ="single"
+console.log(First_Name);
+console.log(Last_Name)
+console.log(country);
+console.log(marital_status);
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+console.log(First_Name,Last_Name,marital_status,country,Age);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge="30"
+let yourAge="60"
+console.log(myAge,yourAge);
